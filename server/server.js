129c516c47d127a3ce8d362e408c184d95f8bcf2
akaman9250 <marshmallow9250@gmail.com>
2026-05-12 07:16:53 +0530

    second

--- server/server.js
+++ server/server.js
@@ -55,7 +55,7 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 if (process.env.NODE_ENV !== 'test') {
-  app.listen(PORT, () => {
+  app.listen(PORT, '0.0.0.0', () => {
     console.log(`🚀 Server running on port ${PORT}`);
   });
 }
