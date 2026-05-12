eb87f1b93ade9acf6653c124f4fcdd653369ec80
akaman9250 <marshmallow9250@gmail.com>
2026-05-12 07:19:36 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -16,9 +16,9 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
-  credentials: true,
-}));
+   origin: process.env.CLIENT_URL || 'http://localhost:5173',
+   credentials: true,
+ }));
 app.use(express.json());
 
 // Routes
